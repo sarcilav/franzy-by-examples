c33966aa8686356037804f7c3532bb88419a9992
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2018-03-05 21:58:55 +0100

    Add franzy deps

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-kafka.franzy/core "2.0.7"]
+                 [clj-kafka.franzy/common "2.0.7"]
+                 [clj-kafka.franzy/nippy "2.0.7"]
+                 [clj-kafka.franzy/json "2.0.7"]
+                 [clj-kafka.franzy/examples "2.0.7"]]
   :main ^:skip-aot example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
